e36894088ce5edad04db84bd3c044f4e8286baf2
detteiu <detteiu.stg@gmail.com>
2020-07-30 08:57:17 +0900

    #1 feat: tokenを設定できるようにする

--- app/app.py
+++ app/app.py
@@ -1,4 +1,12 @@
+import os
+import sys
 import MyClient
 
+try:
+    token = os.environ['DICE_BOT_TOKEN']
+except KeyError:
+    print('環境変数「DICE_BOT_TOKEN」を設定してください')
+    sys.exit()
+
 client = MyClient.MyClient()
-client.run('token')
+client.run(token)
