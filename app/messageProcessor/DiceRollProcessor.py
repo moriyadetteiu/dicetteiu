162382af7afb29ffe19ee8addea11678342f5928
detteiu <detteiu.stg@gmail.com>
2020-07-30 13:18:40 +0900

    #13 feat: 結果を表示できるようにする

--- app/messageProcessor/DiceRollProcessor.py
+++ app/messageProcessor/DiceRollProcessor.py
@@ -1,3 +1,4 @@
+import collections
 import re
 import random
 import discord
@@ -13,6 +14,7 @@ class DiceRollProcessor(AbstractProcessor.AbstractProcessor):
         parsed_content = self.parse(content)
 
         results = []
+        compare_results = []
         for i in range(parsed_content['number_of_dice']):
             result = random.randint(1, parsed_content['maximum_number'])
             modify_operator = parsed_content['result_modify_operator']
@@ -22,20 +24,39 @@ class DiceRollProcessor(AbstractProcessor.AbstractProcessor):
                 modify_operator,
                 modify_number
             )
-            if result != modified_result:
-                modified_result = f'({result}) {modify_operator} {modify_number} = {modified_result}'
-            results.append(str(modified_result))
+            modified_result_text = (
+                f'({result}) {modify_operator} {modify_number} = {modified_result}'
+                if result != modified_result
+                else str(modified_result)
+            )
+            results.append(modified_result_text)
+            compare_result = self.compare_result(
+                modified_result,
+                parsed_content['compare_method'],
+                parsed_content['compare_number']
+            )
+            if compare_result != '':
+                compare_results.append(compare_result)
 
         responce_message = ', '.join(results)
+        compare_result_aggregates = collections.Counter(compare_results)
+        compare_result_texts = []
+        for result, count in compare_result_aggregates.items():
+            compare_result_texts.append(f'{result}: {count}個')
+        if len(compare_result_texts) > 0:
+            responce_message += '\n' + '\n'.join(compare_result_texts)
+
         await message.channel.send(responce_message)
 
     def parse(self, content):
-        result = re.match(r'^([0-9]+)d([0-9]+) *([+-/*/]*) *([0-9]*)', content)
+        result = re.match(r'^([0-9]+)d([0-9]+) *([+-/*/]*) *([0-9]*) *([><=])* *([0-9]*)', content)
         return {
             'number_of_dice': int(result.group(1)),
             'maximum_number': int(result.group(2)),
             'result_modify_operator': result.group(3),
             'result_modify_number': result.group(4),
+            'compare_method': result.group(5),
+            'compare_number': result.group(6),
         }
 
     def modify_roll_result(self, result: int, operator: str, number: str) -> int:
@@ -50,6 +71,23 @@ class DiceRollProcessor(AbstractProcessor.AbstractProcessor):
             return result * number
         if operator == '/':
             return result / number
+
+    def compare_result(self, result: int, method: str, number: str) -> str:
+        if method == '' or number == '':
+            return ''
+        succeed = None
+        number = int(number)
+        if method == '>':
+            succeed = result > number
+        if method == '>=':
+            succeed = result >= number
+        if method == '<':
+            succeed = result < number
+        if method == '<=':
+            succeed = result <= number
+        if succeed == None:
+            return ''
+        return '成功' if succeed else '失敗'
 
     def is_enable(self, message: discord.Message):
         channel_id = int(message.channel.id)
