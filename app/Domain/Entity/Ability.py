ad676d581297ef8ef858416a8b157551b4b21a17
detteiu <detteiu.stg@gmail.com>
2020-08-12 19:24:02 +0900

    #20 feat: Abilityクラス、テストを実装

---
+++ app/Domain/Entity/Ability.py
@@ -0,0 +1,11 @@
+from Domain.Exceptions.InvalidArgumentException import InvalidArgumentException
+
+class Ability():
+    def __init__(self, name: str):
+        if len(name) == 0:
+            raise InvalidArgumentException('技能名は1文字以上にしてください')
+
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
