55078a8daa8c08a4fa9213946abdac09226c7aa6
detteiu <detteiu.stg@gmail.com>
2020-08-12 19:43:22 +0900

    #20 feat: OwnedAbilityクラス、テストの実装

---
+++ app/Domain/Entity/OwnedAbility.py
@@ -0,0 +1,17 @@
+from Domain.Exceptions.InvalidArgumentException import InvalidArgumentException
+from Domain.ValueObject.Dice import Dice
+from .Ability import Ability
+
+class OwnedAbility:
+    def __init__(self, ability: Ability, level: int):
+        if level < 0 or level > 100:
+            raise InvalidArgumentException('技能の熟練度は0～100にしてください')
+
+        self.__ability = ability
+        self.__level = level
+
+    def get_ability(self):
+        return self.__ability
+
+    def get_level(self):
+        return self.__level
