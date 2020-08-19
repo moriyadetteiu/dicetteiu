77ff637ef1ee533f8c315cb18b59936ff4494ed9
detteiu <detteiu.stg@gmail.com>
2020-08-20 04:56:16 +0900

    #27 fix: 小文字のディレクトリを削除

--- app/messageProcessor/AbstractProcessor.py
+++
@@ -1,7 +0,0 @@
-from abc import *
-import discord
-
-class AbstractProcessor(metaclass=ABCMeta):
-    @abstractmethod
-    async def process(self, message: discord.Message):
-        pass
--- app/messageProcessor/MessageContentException.py
+++
@@ -1,2 +0,0 @@
-class MessageContentException(Exception):
-    pass
